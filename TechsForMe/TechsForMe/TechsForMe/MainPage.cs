f6d500f0ffd8877e783b5510462d0ec68e6ac6bd
cmauer69 <cmauer@mauerenterprises.com>
2017-04-24 19:02:10 -0500

    Working on the login screen

---
+++ TechsForMe/TechsForMe/TechsForMe/MainPage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xamarin.Forms;
+
+using TechsForMe.Views;
+
+namespace TechsForMe.LoginNavigation
+{
+    public class MainPageCS : ContentPage
+    {
+        public MainPageCS()
+        {
+            var toolbarItem = new ToolbarItem
+            {
+                Text = "Logout"
+            };
+            toolbarItem.Clicked += OnLogoutButtonClicked;
+            ToolbarItems.Add(toolbarItem);
+            Title = "Main Page";
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label
+                    {
+                        Text = "Main app content goes here",
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.CenterAndExpand
+                    }
+                }
+            };
+
+            //Content = new TabbedPage()
+            //App.Current.MainPage = new TabbedPage
+            //{
+            //    Children = {
+            //         new NavigationPage(new ItemsPage())
+            //        {
+            //            Title = "Browse",
+            //            Icon = Device.OnPlatform("tab_feed.png",null,null)
+            //        },
+            //        new NavigationPage(new AboutPage())
+            //        {
+            //            Title = "About",
+            //            Icon = Device.OnPlatform("tab_about.png",null,null)
+            //        },
+            //   }
+            //};
+        }
+
+        async void OnLogoutButtonClicked(object sender, EventArgs e)
+        {
+            App.IsUserLoggedIn = false;
+            Navigation.InsertPageBefore(new LoginPageCS(), this);
+            await Navigation.PopAsync();
+        }
+    }
+}
